70ac76e588c7ffcabf5a199fc02b8a6ea3b97e8f
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-03-29 20:42:02 -0400

    update problem 7

--- problem7.clj
+++ problem7.clj
@@ -3,3 +3,9 @@
 		(< n 2) false
 		(< n 4) true
 		:else (not= 0 (reduce *' (map #(mod n %) (range 2 (+ 1 (Math/sqrt n))))))))
+
+(defn nth-prime [n]
+	(nth (filter is-prime? 
+		(range)) (dec n)))
+
+(nth-prime 10001)
